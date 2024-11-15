349bbd9709c8d014749c00e3f027a84bfa17cb81
Taron Gukasyan <funkadelica@mail.ru>
2024-11-15 19:27:50 +0300

    changed hibernate data

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -9,28 +9,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "westside";
-
-
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            connection.setAutoCommit(false);
-
-
-        } catch (ClassNotFoundException | SQLException e) {
-
-            System.out.println(e.getMessage());
-
-        }
-
-        return connection;
-    }
 
     public static SessionFactory getSessionFactory() {
         Configuration configuration = new Configuration();
